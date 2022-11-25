368ee4c11ad188d187d1b9e4ace50e1919b4a5b5
Rustam <pkheshkhov0707@mail.ru>
2022-11-25 19:25:33 +0300

    задача 4

---
+++ задача 4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число ");
+string? firstnumber = Console.ReadLine() ;
+int NumberA = Convert.ToInt32(firstnumber);
+Console.WriteLine("Введите второе число ");
+string? secondnumber = Console.ReadLine();
+int NumberB = Convert.ToInt32(secondnumber);
+Console.WriteLine("введите третье");
+string? thirdnumber = Console.ReadLine();
+int NumberC = Convert.ToInt32(thirdnumber);
+int maxNumber;
+if (NumberB > NumberA)
+{
+     maxNumber = NumberB;
+} else maxNumber = NumberA;
+if (NumberC > maxNumber)
+ {
+    maxNumber=NumberC;
+ } else maxNumber = maxNumber;
+
+ Console.WriteLine(maxNumber);
