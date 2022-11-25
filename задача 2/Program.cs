6e3b95fc7d2a97693a57cca79f93e8516527151f
Rustam <pkheshkhov0707@mail.ru>
2022-11-25 18:36:14 +0300

    initial commit

---
+++ задача 2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число ");
+string? firstnumber = Console.ReadLine() ;
+int NumberA = Convert.ToInt32(firstnumber);
+Console.WriteLine("Введите второе число ");
+string? secondnumber = Console.ReadLine();
+int NumberB = Convert.ToInt32(secondnumber);
+int maxNumber;
+if (NumberA > NumberB)
+{
+    maxNumber=NumberA;
+} else {
+    maxNumber=NumberB;
+ } 
+ Console.WriteLine(maxNumber);
