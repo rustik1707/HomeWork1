44ed78e065844f083fd6c3aeb6adb27cfddd67af
Rustam <pkheshkhov0707@mail.ru>
2022-11-25 19:39:10 +0300

    задача 6

---
+++ задача 6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите число");
+string?number = Console.ReadLine() ;
+int NumberA = Convert.ToInt32(number);
+
+if (NumberA%2 == 0)
+{ 
+    Console.WriteLine("четное число");
+} else { 
+    Console.WriteLine("нечетное число");
+} 
