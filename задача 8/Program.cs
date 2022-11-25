880ea95c4264aa051840de21ca84e4c32882bd20
Rustam <pkheshkhov0707@mail.ru>
2022-11-25 20:05:10 +0300

    задача 8

---
+++ задача 8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Напишите число N");
+string?numberN = Console.ReadLine() ;
+int Number = Convert.ToInt32(numberN);
+int i = 2;
+while (i<=Number)
+{ Console.WriteLine(i);
+i = i+2;
+}
+
+    
